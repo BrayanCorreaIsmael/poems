82a6eabf3f8f723e58aa47821c38f600010bd8dc
BrayanCorreaIsmael <briancorrea1@yahoo.com.ar>
2023-10-22 14:23:22 -0300

    Se agrego mucho el alma y decorados

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,114 @@
 import React from 'react'
 import './Home.css'
+import PoemList from './PoemList'; 
+
 
 function Home() {
+  const products = [
+    {
+      nombre: 'Fredy Jiminez ',
+      id: 0,
+      color: 'Marron',
+      poema: 'Ella tiene una sonrisa tan dulce, que ya no necesita ponerle azucar al cafe de sus ojos',
+  },
+  {
+      nombre: 'Anonimo ',
+      id: 1,
+      color: 'Azul',
+      poema: 'El tiempo es relativo, tal vez nuestro amor duro unos dias en nuestra vida o talvez nos amamos toda una vida, en solo unos dias ',
+  },
+  {
+      nombre: 'Anonimo ',
+      id: 2,
+      color: 'Turquesa',
+      poema: 'Eres la única persona que tiene la dicha de saber lo que se siente mi amor; de sentir el fuego que emane este roto corazón. De conocer el latir en mi pecho a la perfección. Eres la única persona en saber lo que la luna, el mar y el atardecer provocan en mi esternón. ¿Por qué la vida es asi de fugaz? Si se supone que hicimos todo bien, ¿Cómo es posible que la luna no brille más, que las estrellas comiencen a caer al azar y que de pronto, en tu mente mi cuerpo ya no esté más? Curiosa la forma en que la vida dijo no, que por más que haya luchado por este amor, el cielo me miro y de pastel jamás se pintó. Eras mi vida y ahora, mi vida, ¿Qué haré con tanto pesar en el pecho? '
+  },
+  {
+      nombre: 'Mario Benedetti  ',
+      id: 3,
+      color: 'Anaranjado',
+      poema:'Mi estrategia es que un día cualquiera no sé cómo ni sé con que pretexto por fin me necesites.'
+  },
+  {
+      nombre: 'Charles Bukowski ',
+      id: 4,
+      color: 'Azul',
+      poema: 'La belleza es relativa. Muchos creen que tienen a la mujer más bonita cuando todos se vuelven a mirarla. Yo creo que tienes a la más hermosa cuando ya no quieres mirar a ninguna otra. '
+  },
+  {
+      nombre: 'Charles Bukowski ',
+      id: 5,
+      color: 'Gris',
+      poema: 'Que te rompan el corazón está bien, al final a todos nos gusta montar rompecabezas... pero que se lleven las piezas, eso sí es jodido.'
+  },
+  {
+      nombre: 'Anonimo',
+      id: 6,
+      color: 'Azul ',
+      poema: 'Yo la miraba y puedo jurar que no me quedaban ganas de mirar a nadie más. En ese preciso momento comprendí que los ojos; siempre, siempre le pertenecen a la persona que los hace brillar.'
+  },
+  {
+      nombre: 'Desconocido',
+      id: 7,
+      color: 'Marron',
+      poema: 'Me perdí en su mirada, es que el color de sus ojos me encantaban; no eran ni azules, ni verdes. Eran color café, café que quita el sueño, café que produce desvelo.'
+  },
+  {
+      nombre: 'Juan Carlos Onetti ',
+      id: 8,
+      color: 'Azul',
+      poema:'La maniática tarea de construir eternidades con elementos hechos de fugacidad, tránsitos y olvido. '
+  },
+  {
+      nombre: 'Julio Cortázar ',
+      id: 9,
+      color: 'Anaranjado',
+      poema:'Porque, sin buscarte te ando encontrando por todos lados, principalmente cuando cierro los ojos. '
+  },
+  {
+      nombre: 'Mario Benedetti',
+      id: 10,
+      color: 'Rojo',
+      poema:'Te quiero como para llevarte a mis lugares favoritos y contarte que es ahí donde me siento a buscarte en la niebla de miradas que no son tuyas, pero aún así te busco.'
+  },
+  {
+      nombre: 'Frida Kahlo',
+      id: 11,
+      color: 'Azul',
+      poema:'Porque la prefieres a ella, si ni el color de tus ojos sabe, cuando yo he contado cada una de tus pestañas. '
+  },
+  {
+      nombre: 'Mario Benedetti ',
+      id: 12,
+      color: 'Verde',
+      poema:'Si no tienes donde quedarte, tengo un espacio disponible en la parte izquierda de mi pecho... Disculpa el desorden, la última persona se fue sin limpiar. '
+  },
+  {
+      nombre: 'Mario Benedetti ',
+      id: 13,
+      color: 'Verde',
+      poema:'Ella no es mía tampoco soy suyo. Lo nuestro es temporal, somos un préstamo voluntario de momentos inolvidables que quizá podría durar toda la vida. '
+  }
+
+
+    // ... tus datos de poemas
+  ];
   return (
-    <div className="home">
-      
-      
-        <h2>Titulo y Autor</h2>
-        <p className="poem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sequi explicabo earum facere corporis? Nihil aliquid labore, modi eaque soluta omnis rerum blanditiis, nulla recusandae libero commodi quisquam aspernatur exercitationem tenetur incidunt, quaerat quia consequuntur natus esse dolorem? Quae maiores nisi commodi aliquam quas. Laudantium maiores vero soluta voluptatum mollitia, modi dolores accusantium, dolore amet necessitatibus aperiam? Pariatur minus magnam beatae ipsum veniam quibusdam, voluptate nobis nostrum ea laboriosam? Dicta laboriosam explicabo animi molestiae maiores quidem quam ullam minus voluptas architecto excepturi eos impedit, culpa quisquam et sequi, beatae fugit asperiores. Asperiores dignissimos earum quas inventore, libero voluptatum officia ducimus neque architecto deleniti sapiente delectus, vitae, quaerat fuga consectetur nulla laboriosam quidem quo perspiciatis incidunt eos eligendi rerum eaque ratione! Quibusdam ipsam sit pariatur possimus fugiat in nam doloremque fuga, minus accusamus dolorum ad laborum amet asperiores, alias quaerat? Sequi, animi at nam magnam iure, odio qui earum natus illo iusto nesciunt consequatur quibusdam voluptate voluptates blanditiis molestias ea. Illo, sapiente nemo ab earum molestiae minus nostrum cum veniam placeat aspernatur, non quos voluptatibus sit itaque! Deserunt eligendi exercitationem ex excepturi officia! Facilis possimus ipsa libero dolore laudantium ab maxime id ipsum, recusandae laborum eligendi. Maiores doloremque praesentium ea officia.</p>
-    <aside>
-        Banner/Aside -Ta mal ubicado-
-    </aside>
+    <div className="App">
+      <PoemList poems={products} />
+  </div>
     
-    </div>
+    // <div className="home">
+      
+      
+    //     <h2>Titulo y Autor</h2>
+    //     <p className="poem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sequi explicabo earum facere corporis? Nihil aliquid labore, modi eaque soluta omnis rerum blanditiis, nulla recusandae libero commodi quisquam aspernatur exercitationem tenetur incidunt, quaerat quia consequuntur natus esse dolorem? Quae maiores nisi commodi aliquam quas. Laudantium maiores vero soluta voluptatum mollitia, modi dolores accusantium, dolore amet necessitatibus aperiam? Pariatur minus magnam beatae ipsum veniam quibusdam, voluptate nobis nostrum ea laboriosam? Dicta laboriosam explicabo animi molestiae maiores quidem quam ullam minus voluptas architecto excepturi eos impedit, culpa quisquam et sequi, beatae fugit asperiores. Asperiores dignissimos earum quas inventore, libero voluptatum officia ducimus neque architecto deleniti sapiente delectus, vitae, quaerat fuga consectetur nulla laboriosam quidem quo perspiciatis incidunt eos eligendi rerum eaque ratione! Quibusdam ipsam sit pariatur possimus fugiat in nam doloremque fuga, minus accusamus dolorum ad laborum amet asperiores, alias quaerat? Sequi, animi at nam magnam iure, odio qui earum natus illo iusto nesciunt consequatur quibusdam voluptate voluptates blanditiis molestias ea. Illo, sapiente nemo ab earum molestiae minus nostrum cum veniam placeat aspernatur, non quos voluptatibus sit itaque! Deserunt eligendi exercitationem ex excepturi officia! Facilis possimus ipsa libero dolore laudantium ab maxime id ipsum, recusandae laborum eligendi. Maiores doloremque praesentium ea officia.</p>
+    // <aside>
+    //     Banner/Aside -Ta mal ubicado-
+    // </aside>
+    
+    // </div>
+    
 
   )
 }
---
+++ src/components/Home/PoemList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './PoemList.css'
+
+
+
+const PoemList = ({ poems}) => {
+  return (
+    <div className='Contenedor'>
+      <h2>Colores</h2>
+      {poems.map((poem) => (
+        <div key={poem.id} className={poem.color} id='contenido'>
+          <p className='Encabezado'> {poem.nombre} | {poem.color}</p>           
+          <p className='Alma'>{poem.poema}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PoemList;
